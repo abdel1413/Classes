acd099db5fdd72aff0261f176f1be3df0f76a28f
Your Name <you@example.com>
2022-08-29 23:34:52 -0400

    created a class called matring

--- MatrixIterator.js
+++ MatrixIterator.js
@@ -4,21 +4,25 @@ class MatrixIterator {
         this.y = 0;
         this.matrix = matrix
     }
+    //check if the y reached the bottom of the matrix if not
     next() {
         if (this.y === this.matrix.height) {
             return {done:true}
         }
+
+
+        // create an object holding the current value .  
         let value = {
             x: this.x,
             y: this.y,
-            value = this.matrix.get(this.x,this.y)
+            value = this.matrix.get(this.x,this.y) 
         }
-        this.x++;
+        this.x++;  //and then update its position moving to the next row if possoble
         if (this.x === this.matrix.width) {
-            this.x = 0;
-            this.y++
+            this.x = 0;  //reset the x 
+            this.y++    //update the y 
         }
-        return {done:false}
+        return {done:false}    //done the iteration nomore element in the objec
     }
     
 }
